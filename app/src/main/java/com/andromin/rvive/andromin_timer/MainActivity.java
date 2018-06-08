206c48399e09cfcc3f1cbbf8d53db4d3884b5850
Vivek <viveknarayanan15082@cse.ssn.edu.in>
2018-06-08 11:26:16 +0530

    " Added comments "

--- app/src/main/java/com/andromin/rvive/andromin_timer/MainActivity.java
+++ app/src/main/java/com/andromin/rvive/andromin_timer/MainActivity.java
@@ -19,8 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void start(View v)
     {
+        //----Timer function
+        //1 - Create a new timer
         Timer t = new Timer();
+
+        //2 - Find the view, here we are using a button
         final Button btn = (Button)findViewById(R.id.btn);
+
+        //3 - Create the timer task, that is
+        //    define the work to be performed
         TimerTask tt = new TimerTask() {
             @Override
             public void run()
@@ -29,9 +36,15 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+
+        //4 - Add the task to the time and schedule it
+        //    A - First parameter is the TimerTask
+        //    B - Second is the delay, that is after
+        //    how long your execution starts in ms
+        //    C - Third parameter is the time period
+        //    after which the task executes
         t.schedule(tt,0,1000);
 
     }
-
 
 }
